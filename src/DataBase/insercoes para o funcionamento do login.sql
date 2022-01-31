d1f646e7e8887e3fc393a33d59af26c7c0609887
Neusia Hilario <neusiahilariomassingue@gmail.com>
2022-01-31 21:28:59 +0200

    SEDV-9-implementar-o-login-usando-as-novas-interfaces-do-java-fx, e atualizacao do ficheiro em sql sedv_mydb_v_1.0,

---
+++ src/DataBase/insercoes para o funcionamento do login.sql
@@ -0,0 +1,10 @@
+
+insert  into previlegio (prodPodeRegistar, prodPodeAlterarDados, ProdPodeAdicionarStock, 
+ProdPodeRegistarCateg, ProdPodeRegistarUnida, relVendasRecentes, relVendasPorDaata, relProdutosMaisVendidos)
+ values ('nao','nao','sim','sim','sim','sim','sim','sim');
+
+
+insert  into `usuario` values (7,'Admin','1234','Gerente','nao'),(8,'user','0000','Funcionario','nao');
+
+
+insert  into `categoria` values (13,'Alimentares'),(14,'Cosmeticos'),(15,'Limpeza'),(16,'Outro');
